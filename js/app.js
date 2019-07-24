a800003caab40a4ddaedbe7b0d9e2599d3926230
Boris Terraza G <ingterraza@gmail.com>
2019-07-24 17:32:19 -0500

    Commit inicial Development

---
+++ js/app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems, 'left');
+});
+
+// Or with jQuery
+
+$(document).ready(function () {
+  $('.dropdown-trigger').dropdown();
+});
+
