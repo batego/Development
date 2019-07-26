5519be0df96dd0e0e4606657243a9a17d984b32e
Boris Terraza G <ingterraza@gmail.com>
2019-07-26 12:55:07 -0500

    Rutas para el servidor en Express

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 var express = require('express');
 var app = express();
+
+//un parametro ej: localhost:3000/usuario?user:dereck
+app.get('/usuario/:user', function (req, res) {
+    res.send(`hola ${req.params.user}, soy un servidor`);
+});
+
+//params + data  Ej: localhost:3000/autores/boris/cienañosdesoledad
+app.get('/autores/:autor/:libro', function (req, res) {
+    res.send(`Autor ${req.params.autor}, Libro: ${req.params.libro}`);
+});
+
+//params + data  Ej: localhost:3000/autores/boris?ano=2019
+app.get('/autores/:autor', function (req, res) {
+    res.send(`Autor ${req.params.autor}, Año: ${req.query.ano}`);
+});
+
 app.get('/', function(req, res){
     res.send('Hola Mundo');
 });
